6dc348a57ead5d17e5ff7d4845e6a754ff83851e
jiho1996 <jiho.urssu@gmail.com>
2021-12-01 15:05:22 +0900

    initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+let order = 3
+
+let defeatCount = new Array(order)
+        for (let i = 0; i < order; i++){
+            defeatCount[i] = 3
+        }
+for (let i = 0; i < 3; i++){       
+    console.log(defeatCount[i])
+}
